ee932295c50da9ba4ffa27a05b0412046bb40b0f
winnie.byun <winnie.byun@groundx.xyz>
2021-01-22 13:58:00 +0900

    put data in 'data'

--- request-getTime.sh
+++ request-getTime.sh
@@ -1,8 +1,10 @@
 curl --location --request POST 'http://localhost:8080/' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "address": "0x57cefc59ae8e882d9a0334831d59778611c32f2b",
-    "functionSelector": "557ed1ba",
-    "dataPrefix": "0x",
-    "value": "0x"
+    "data": {
+        "address": "0x57cefc59ae8e882d9a0334831d59778611c32f2b",
+        "functionSelector": "0x557ed1ba",
+        "dataPrefix": "0x",
+        "result": "0x"
+	}
 }'
--- request-setTime.sh
+++ request-setTime.sh
@@ -1,8 +1,10 @@
 curl --location --request POST 'http://localhost:8080/' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "address": "0x57cefc59ae8e882d9a0334831d59778611c32f2b",
-    "functionSelector": "0x3beb26c4",
-    "dataPrefix": "0x000000000000000000000000000000000000000000000000000000000",
-    "value": "0x187fe1"
+    "data": {
+        "address": "0x57cefc59ae8e882d9a0334831d59778611c32f2b",
+        "functionSelector": "0x3beb26c4",
+        "dataPrefix": "0x000000000000000000000000000000000000000000000000000000000",
+        "value": "0x187fe1"
+    }
 }'
